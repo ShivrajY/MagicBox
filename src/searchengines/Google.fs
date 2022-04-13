341f1f5f169c3cc5729be60d4bade67e74a3170b
shivrajy <shivrajy@gmail.com>
2022-04-13 20:59:52 +0530

    .

---
+++ src/searchengines/Google.fs
@@ -0,0 +1,13 @@
+namespace Magicbox.SearchEngine
+open FSharp.Data
+module Google =
+    let Search url = 
+        let doc = HtmlDocument.Load url
+        let searchResults = doc.Descendants ["a"]
+                    |> Seq.choose(fun x -> 
+                            x.tryGetAttribute("href")
+                            |> Option.map(fun a-> x.InnerText(), a.Value()))
+                    |>Seq.toList
+                    |>List.filter(fun(name, url) -> name<>"Cached" && name <> "Similar" && url.StartsWith("/url?"))
+                    |>List.map(fun(name, url) -> name, url.Substring(0, url.IndexOf("&sa=")).Replace("/url?q=",""))
+        searchResults
